1e2806d531e5bd5b6c88c69eb46bf3714b5cf0b2
lht1949 <liuht0817@gmail.com>
2020-06-05 14:48:38 -0400

    initial push app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# year = st.slider('year of purchase', min_value=2015, max_value=2019, value=2015)
+# st.markdown(f"your laptop was purchase in {year}")
+
+# Draw a title and some text to the app:
+'''
+# Smart Price
+
+Optimum price for your used laptop!
+'''
